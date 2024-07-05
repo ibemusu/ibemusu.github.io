1fd661d4d95087486fce1fa3e4da5802633d5c09
ibemusu <121378608+ibemusu@users.noreply.github.com>
2024-07-05 15:09:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Function to update the current time in UTC
+    function updateTime() {
+        const now = new Date();
+        const utcDate = now.toISOString().split('T')[0]; // Extracts the date part of the ISO string (YYYY-MM-DD)
+        const utcTime = now.toUTCString().split(' ')[4];
+        document.querySelector("[ data-testid='currentTimeUTC']").textContent = utcTime;
+    }
+
+    function updateDay() {
+        const now = new Date();
+        const utcDate = now.toISOString().split('T')[0]; // Extracts the date part of the ISO string (MM-DD-YYYY)
+        document.querySelector("[data-testid='CurrentUTCdate']").textContent = utcDate;
+    }
+    // Function to update the current day of the week
+    function updateDay() {
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const now = new Date();
+        const day = days[now.getUTCDay()];
+        document.querySelector("[data-testid='currentDay']").textContent = day;
+    }
+    
+    // Initial update
+    updateTime();
+    updateDay();
+
+    // Update time every second
+    setInterval(updateTime, 1000);
+});
